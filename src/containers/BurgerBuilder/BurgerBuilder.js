2fa9b571bfc7dcc2ab6c6870c9fd1c429d185589
Michał Rychlewicz <michalrychlewicz@gmail.com>
2018-04-29 21:06:38 +0200

    Added Burger ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+
 import Aux from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-	render () {
-		return (
-			<Aux>
-				<div>Burger</div>
-				<div>Build Controls</div>
-			</Aux>
-		);
-	}
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Burger ingredients={this.state.ingredients} />
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
 }
 
 export default BurgerBuilder;
